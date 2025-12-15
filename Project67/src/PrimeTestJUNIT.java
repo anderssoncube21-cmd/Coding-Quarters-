f5faa5f4800cbfd458ad9dd89583010999effd68
anderssoncube21-cmd <anderssoncube21@gmail.com>
2025-12-15 00:42:35 -0500

    Add files via upload

---
+++ Project67/src/PrimeTestJUNIT.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PrimeTestJUNIT {
+
+	/**
+	 * Test method, feels fun when I see green.
+	 */
+	@Test
+	public void testPrime() {
+		assertTrue(PrimeNumberMethod.isPrime(17));
+	}
+	
+	/**
+	 * Test method, feels fun when I see green.
+	 */
+	@Test
+	public void testNotPrime() {
+		assertFalse(PrimeNumberMethod.isPrime(18));
+	}
+}
---
+++ Project67/src/StringTESTJUNIT.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/*
+* Class: CMSC201
+* Instructor:
+* Description: (Give a brief description for each Program)
+* Due: 12/08/25
+* I pledge that I have completed the programming assignment independently.
+I have not copied the code from a student or any source.
+I have not given my code to any student.
+Print your Name here: Bryan Gonzalez
+*/
+
+
+/**
+ * JUnit test for string.
+ */
+public class StringTESTJUNIT {
+
+	/**
+	 * Test method, feels fun when I see green.
+	 */
+	@Test
+	public void m1() {
+		String str = "Hello, World!";
+		assertEquals(13, str.length());
+	}
+	
+	/**
+	 * Test method, feels fun when I see green.
+	 */
+	@Test
+	public void m2() {
+		String str = "Hello Mr Professor, I only like java when it's not toooo hard.";
+		assertEquals("java", str.substring(32, 36));
+	}
+	/**
+	 * Test method, feels fun when I see green.
+	 */
+	@Test
+	public void m3() {
+		String str = "a";
+		assertEquals('a', str.charAt(0));
+	}
+	/**
+	 * Test method, feels fun when I see green.
+	 */
+	@Test
+	public void m4() {
+		String str = "CMSC 201 is kinda fun";
+		assertEquals(5, str.indexOf("201"));
+	}
+}
+
